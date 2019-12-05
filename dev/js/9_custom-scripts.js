b951be3afce4f3d440260fc378d1ef7c5e14411a
childressdev <jcampbell@childressagency.com>
2019-12-05 09:15:26 -0500

    added swiper js

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -3,5 +3,11 @@
 */
 
 jQuery(document).ready(function($){
-
+  var testimonials = new Swiper('.swiper-container',{
+    loop: true,
+    navigation:{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+  });
 });
