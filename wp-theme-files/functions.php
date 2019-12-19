f61da349f98694101eed00e932ab22395d23d17b
childressdev <jcampbell@childressagency.com>
2019-12-19 17:56:05 -0500

    added

--- wp-theme-files/functions.php
+++ wp-theme-files/functions.php
@@ -67,11 +67,6 @@ function cai_add_script_meta($tag, $handle){
 add_action('wp_enqueue_scripts', 'cai_styles');
 function cai_styles(){
   wp_register_style(
-    'google-fonts',
-    'https://fonts.googleapis.com/css?family=Maitree:400,700|Nunito+Sans:400,600,700|Nunito:700'
-  );
-
-  wp_register_style(
     'fontawesome',
     'https://use.fontawesome.com/releases/v5.6.3/css/all.css'
   );
@@ -81,7 +76,6 @@ function cai_styles(){
     get_stylesheet_directory_uri() . '/style.css'
   );
 
-  wp_enqueue_style('google-fonts');
   wp_enqueue_style('fontawesome');
   wp_enqueue_style('cai-css');
 }
@@ -102,14 +96,67 @@ function cai_setup(){
   add_theme_support('post-thumbnails');
   //set_post_thumbnail_size(320, 320);
 
+  add_theme_support(
+    'html5',
+    array(
+      'comment-form',
+      'comment-list',
+      'gallery',
+      'caption'
+    )
+  );
+
+  add_theme_support('editor-styles');
+  add_editor_style('style-editor.css');
+
+  add_theme_support('wp-block-styles');
+  add_theme_support('responsive-embeds');
+
   register_nav_menus(array(
     'header-nav' => 'Header Navigation',
-    'footer-nav' => 'Footer Navigation',
-    'company-menu' => 'Company Footer Menu',
-    'services-menu' => 'Services Footer Menu'
+    'services-sub-nav' => 'Services Sub Navigation',
+    'services-nav' => 'Services Footer Navigation'
   ));
 
   load_theme_textdomain('cai', get_stylesheet_directory_uri() . '/languages');
 }
 
 require_once dirname(__FILE__) . '/includes/class-wp-bootstrap-navwalker.php';
+
+function cai_header_fallback_menu(){ ?>
+  <div id="header-nav" class="collapse navbar-collapse">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item<?php if(is_front_page()){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url()); ?>" class="nav-link" title="<?php echo esc_attr__('Home', 'cai'); ?>"><?php echo esc_html__('Home', 'cai'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('about-us')); ?>">
+        <a href="<?php echo esc_url(home_url('about-us')); ?>" class="nav-link" title="<?php echo esc_attr__('About Us', 'cai'); ?>"><?php echo esc_html__('About Us', 'cai'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('services') || is_singular('service')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('services')); ?>" class="nav-link" title="<?php echo esc_attr__('Services', 'cai'); ?>"><?php echo esc_html__('Services', 'cai'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('case-studies') || is_singular('case-study')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('case-studies')); ?>" class="nav-link" title="<?php echo esc_attr__('Case Studies', 'cai'); ?>"><?php echo esc_html__('Case Studies', 'cai'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_home() || is_singular('post')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('news-events')); ?>" class="nav-link" title="<?php echo esc_attr__('News/Events', 'cai'); ?>"><?php echo esc_html__('News/Events', 'cai'); ?></a>
+      </li>
+      <li class="nav-item<?php if(is_page('contact')){ echo ' active'; } ?>">
+        <a href="<?php echo esc_url(home_url('contact')); ?>" class="nav-link" title="<?php echo esc_attr__('Contact', 'cai'); ?>"><?php echo esc_html__('Contact', 'cai'); ?></a>
+      </li>
+    </ul>
+  </div>
+<?php }
+
+function cai_services_sub_nav_fallback_menu(){ ?>
+  <div id="services-nav">
+    <nav class="nav services-nav">
+      <a href="<?php echo esc_url(home_url('brand-identity')); ?>" title="<?php echo esc_attr__('Brand Identity', 'cai'); ?>"><?php echo esc_html__('Brand Identity', 'cai'); ?></a>
+      <a href="<?php echo esc_url(home_url('graphic-design')); ?>" title="<?php echo esc_attr__('Graphic Design', 'cai'); ?>"><?php echo esc_html__('Graphic Design', 'cai'); ?></a>
+      <a href="<?php echo esc_url(home_url('web-design')); ?>" title="<?php echo esc_attr__('Web Design', 'cai'); ?>"><?php echo esc_html__('Web Design', 'cai'); ?></a>
+      <a href="<?php echo esc_url(home_url('seo')); ?>" title="<?php echo esc_attr__('SEO', 'cai'); ?>"><?php echo esc_html__('SEO', 'cai'); ?></a>
+      <a href="<?php echo esc_url(home_url('digital-marketing')); ?>" title="<?php echo esc_attr_('Digital Marketing', 'cai'); ?>"><?php echo esc_html__'Digital Marketing', 'cai'); ?></a>
+      <a href="<?php echo esc_url(home_url('social-media')); ?>" title="<?php echo esc_attr__('Social Media', 'cai'); ?>"><?php echo esc_html__('Social Media', 'cai'); ?></a>
+    </nav>
+  </div>
+<?php }
---
+++ wp-theme-files/header.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <meta http-equiv="cache-control" content="public">
+  <meta http-equiv="cache-control" content="private">
+
+  <title><?php echo esc_html(bloginfo('name')); ?></title>
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php get_template_part('partials/sprites.svg'); ?>
+  <header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <div class="brand">
+        <a href="<?php echo esc_url(home_url()); ?>" class="logo">
+          <svg>
+            <use xlink:href="#logo" />
+          </svg>
+        </a>
+        <a href="<?php echo esc_url(home_url()); ?>" class="brand-name">Childress Agency</a>
+      </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-nav" aria-controls="header-nav" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <?php
+        $header_nav_args = array(
+          'theme_location' => 'header-nav',
+          'menu' => '',
+          'container' => 'div',
+          'container_id' => 'header-nav',
+          'container_class' => 'collapse navbar-collapse',
+          'menu_id' => '',
+          'menu_class' => 'navbar-nav ml-auto',
+          'echo' => true,
+          'fallback_cb' => 'cai_header_fallback_menu',
+          'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+          'depth' => 2,
+          'walker' => new WP_Bootstrap_NavWalker()
+        );
+        wp_nav_menu($header_nav_args);
+       ?>
+    </nav>
+
+    <?php 
+      if(is_page('services') || is_singular('service')){
+        $services_sub_nav_args = array(
+          'theme_location' => 'services-sub-nav', 
+          'menu' => '',
+          'container' => 'div',
+          'container_id' => 'services-nav',
+          'container_class' => '',
+          'menu_id' => '',
+          'menu_class' => 'nav services-nav',
+          'echo' => true,
+          'fallback_cb' => 'cai_services_sub_nav_fallback_menu',
+          'items_wrap' => '<nav id="%1$s" class="%2$s">%3$s</nav>',
+          'depth' => 1,
+        );
+        wp_nav_menu($services_sub_nav_args);
+      }
+    ?>
+  </header>
+
+  <?php if(!is_page('services')): ?>
+    <?php
+      if(is_home()){
+        $blog_page = get_page_by_path('news-events');
+        $blog_page_id = $blog_page->ID;
+
+        $hero_image = get_field('hero_background_image', $blog_page_id);
+        $hero_image_css = get_field('hero_background_image_css', $blog_page_id);
+        $hero_caption = get_field('hero_caption', $blog_page_id);
+      }
+      else{
+        $hero_image = get_field('hero_background_image');
+        $hero_image_css = get_field('hero_background_image_css');
+        $hero_caption = get_field('hero_caption');
+      }
+
+      if(!$hero_image){
+        $hero_image = get_field('default_hero_background_image', 'option');
+        $hero_image_css = get_field('default_hero_background_image_css', 'option');
+      }
+    ?>
+    <section id="<?php echo is_front_page() ? 'hp-hero' : 'hero'; ?>" class="hero" style="background-image:url(<?php echo esc_url($hero_image); ?>);<?php echo esc_attr($hero_image_css); ?>">
+      <div class="container">
+        <div class="hero-caption">
+          <?php if(is_front_page()): ?>
+            <h1><span class="brand">Childress Agency</span>Digital Marketing Made Easy</h1>
+            <a href="<?php echo esc_url(home_url('contact')); ?>" class="btn-main btn-lrg">We're ready when you are.</a>
+          <?php else: ?>
+            <h2><?php echo echo_html($hero_caption); ?></h2>
+          <?php endif; ?>
+        </div>
+      </div>
+      <a href="#contact-page" class="contact-phone"></a>
+      <div class="blue-overlay"></div>
+      <div class="angle-left"></div>
+      <div class="angle-right"></div>
+    </section>
+  <?php endif; ?>
