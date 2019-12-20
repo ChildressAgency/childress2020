992ce3e0cce3256fb25740fe0246b75580882fd1
childressdev <jcampbell@childressagency.com>
2019-12-20 13:54:48 -0500

    added

---
+++ wp-theme-files/front-page.php
@@ -0,0 +1,253 @@
+<?php get_header(); ?>
+  <main id="main">
+    <section id="our-purpose">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="../wp-theme-files/images/ca-graphic.jpg" class="img-fluid d-block mx-auto" alt="CA Graphic" />
+          </div>
+          <div class="col-lg-6">
+            <article>
+              <h2><span class="bold">OUR</span> PURPOSE</h2>
+              <p style="color: #1277d6;"><span class="bold">Our work is produced with a focus on quality and creativity</span> no matter what the scale of the project. Each marketing effort is approached like our firm's flagship project.</p>
+              <a href="#" class="btn-main">About Us</a>
+            </article>
+
+            <article style="margin-top:80px;">
+              <h3><span class="bold">WHY YOU SHOULD HIRE</span> A DIGITAL MARKETING AGENCY.</h3>
+              <ul class="list-unstyled">
+                <li>AVOID FRUSTRATION AND LIMITED RESOURCES</li>
+                <li>CUSTOM DESIGNS THAT BEAT THE COMPETITION EVERY TIME</li>
+                <li>LESS TIME SPENT AT THE OFFICE SCRATCHING YOUR HEAD</li>
+                <li><span style="font-weight: bold; color: #1277d6;">MORE TIME TO BE A BOSS AGAIN...</span></li>
+              </ul>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="what-we-do" style="background-image:url(../wp-theme-files/images/dot-bg.jpg); background-position:center center;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+
+            <div class="row">
+              <div class="col-lg-4 col-md-6 col-12">
+                <a href="#" class="disc-icon-card">
+                  <div class="disc-icon-bg">
+                    <svg class="disc-icon">
+                      <use xlink:href="#icon-pen" />
+                    </svg>
+                  </div>
+                  <h4>graphic design</h4>
+                  <p>a fresh breath of creativity</p>
+                </a>
+              </div>
+              <div class="col-lg-4 col-md-6 col-12">
+                <a href="#" class="disc-icon-card">
+                  <div class="disc-icon-bg">
+                    <svg class="disc-icon">
+                      <use xlink:href="#icon-devices-circle" />
+                    </svg>
+                  </div>
+                  <h4>digital marketing</h4>
+                  <p>certified google partner, marketing consultants</p>
+                </a>
+              </div>
+              <div class="col-lg-4 col-md-6 col-12">
+                <a href="#" class="disc-icon-card">
+                  <div class="disc-icon-bg">
+                    <svg class="disc-icon">
+                      <use xlink:href="#icon-magnifier" />
+                    </svg>
+                  </div>
+                  <h4>brand identity</h4>
+                  <p>builders of bold, beautiful brands</p>
+                </a>
+              </div>
+              <div class="col-lg-4 col-md-6 col-12">
+                <a href="#" class="disc-icon-card">
+                  <div class="disc-icon-bg">
+                    <svg class="disc-icon">
+                      <use xlink:href="#icon-monitor" />
+                    </svg>
+                  </div>
+                  <h4>web design</h4>
+                  <p>bold, beautiful projects</p>
+                </a>
+              </div>
+              <div class="col-lg-4 col-md-6 col-12">
+                <a href="#" class="disc-icon-card">
+                  <div class="disc-icon-bg">
+                    <svg class="disc-icon">
+                      <use xlink:href="#icon-graph" />
+                    </svg>
+                  </div>
+                  <h4>seo</h4>
+                  <p>your primary source of traffic to websites</p>
+                </a>
+              </div>
+              <div class="col-lg-4 col-md-6 col-12">
+                <a href="#" class="disc-icon-card">
+                  <div class="disc-icon-bg">
+                    <svg class="disc-icon">
+                      <use xlink:href="#icon-social-circle" />
+                    </svg>
+                  </div>
+                  <h4>social media</h4>
+                  <p>connect with your consumers</p>
+                </a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-md-5">
+            <div class="v-centered-caption">
+              <h2 class="xtra-lrg-text">WHAT <span class="bold">WE DO</span></h2>
+              <a href="#" class="btn-main">Discover our services</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="clients">
+      <div class="container">
+        <ul class="client-list">
+          <li>
+            <a href="#">
+              <img src="../wp-theme-files/images/client-logo-placeholder.jpg" class="img-fluid d-block mx-auto" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="../wp-theme-files/images/client-logo-placeholder.jpg" class="img-fluid d-block mx-auto" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="../wp-theme-files/images/client-logo-placeholder.jpg" class="img-fluid d-block mx-auto" alt="" />
+            </a>
+          </li>
+        </ul>
+        <a href="#" class="btn-main">Discover our clients</a>
+      </div>
+    </section>
+
+    <section id="testimonials">
+      <div class="container-fluid">
+      <div class="swiper-container">
+
+        <div class="swiper-wrapper">
+          <div class="testimonial swiper-slide">
+            <div class="testimonial-bubble">
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed adola.</p>
+                <cite>&mdash; Gabby Elle</cite>
+              </blockquote>
+            </div>
+          </div>
+          <div class="testimonial swiper-slide">
+            <div class="testimonial-bubble">
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed adola.</p>
+                <cite>&mdash; Gabby Elle</cite>
+              </blockquote>
+            </div>
+          </div>
+          <div class="testimonial swiper-slide">
+            <div class="testimonial-bubble">
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed adola.</p>
+                <cite>&mdash; Gabby Elle</cite>
+              </blockquote>
+            </div>
+          </div>
+          <div class="testimonial swiper-slide">
+            <div class="testimonial-bubble">
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed adola.</p>
+                <cite>&mdash; Gabby Elle</cite>
+              </blockquote>
+            </div>
+          </div>
+          <div class="testimonial swiper-slide">
+            <div class="testimonial-bubble">
+              <blockquote>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed adola.</p>
+                <cite>&mdash; Gabby Elle</cite>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+      </div>
+      <a href="#" class="btn-main">More testimonials</a>
+      </div>
+    </section>
+
+    <section id="stats">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="stat-counter">
+              <div class="counter-wrapper">
+                <!--<ul class="flip-counter default" id="experience-counter"></ul>-->
+                <span class="count years-count">20</span>
+                <span class="counter-plus">+</span>
+              </div>
+              <p>Years of Digital Marketing Experience</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="stat-counter">
+              <div class="counter-wrapper">
+                <!--<ul class="flip-counter default" id="clients-counter"></ul>-->
+                <span class="count clients-count">500</span>
+              </div>
+              <p>Clients & Growing</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="stat-counter">
+              <div class="counter-wrapper">
+                <!--<ul class="flip-counter default" id="industries-counter"></ul>-->
+                <span class="count industries-count">30</span>
+                <span class="counter-plus">+</span>
+              </div>
+              <p>Industries we have worked with</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="news-chats">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 d-flex">
+            <div class="promo-block" style="background-image:url(../wp-theme-files/images/news-bg.jpg); background-position:center center;">
+              <div class="promo-block-caption">
+                <img src="../wp-theme-files/images/news-events-graphic.png" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#" class="btn-main">Click Here</a>
+              </div>
+              <div class="blue-overlay"></div>
+            </div>
+          </div>
+          <div class="col-lg-6 d-flex">
+            <div class="promo-block" style="background-image:url(../wp-theme-files/images/man-talking-to-group.jpg); background-position:center center;">
+              <div class="promo-block-caption">
+                <img src="../wp-theme-files/images/chats-logo.png" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#" class="childress-chats-register"><span>Register</span>for our next chat</a>
+              </div>
+              <div class="blue-overlay"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+<?php get_footer(); 
