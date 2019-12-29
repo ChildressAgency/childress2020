54b1e9a33ed86868a3fb3c3b906354a48b79de06
childressdev <jcampbell@childressagency.com>
2019-12-29 16:43:48 -0500

    finished stats section

--- wp-theme-files/front-page.php
+++ wp-theme-files/front-page.php
@@ -75,59 +75,46 @@
       </section>
   <?php endif; ?>
 
+  <?php if(have_rows('stats')): ?>
     <section id="stats">
       <div class="container">
         <div class="row">
-          <div class="col-lg-4">
-            <div class="stat-counter">
-              <div class="counter-wrapper">
-                <!--<ul class="flip-counter default" id="experience-counter"></ul>-->
-                <span id="years-count" class="count years-count" data-top_number="20"></span>
-                <span class="counter-plus">+</span>
+          <?php while(have_rows('stats')): the_row(); ?>
+
+            <div class="col-lg-4">
+              <div class="stat-counter">
+                <div class="counter-wrapper">
+                  <span class="count odometer" data-top_number="<?php the_sub_field('stat_number'); ?>"></span>
+                  <?php if(get_sub_field('show_plus_sign') == 1): ?>
+                    <span class="counter-plus">+</span>
+                  <?php endif; ?>
+                </div>
+                <p><?php the_sub_field('stat'); ?></p>
               </div>
-              <p>Years of Digital Marketing Experience</p>
             </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="stat-counter">
-              <div class="counter-wrapper">
-                <!--<ul class="flip-counter default" id="clients-counter"></ul>-->
-                <span id="clients-count" class="count clients-count" data-top_number="500"></span>
-              </div>
-              <p>Clients & Growing</p>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="stat-counter">
-              <div class="counter-wrapper">
-                <!--<ul class="flip-counter default" id="industries-counter"></ul>-->
-                <span id="industries-count" class="count industries-count" data-top_number="30"></span>
-                <span class="counter-plus">+</span>
-              </div>
-              <p>Industries we have worked with</p>
-            </div>
-          </div>
+
+          <?php endwhile; ?>
+
         </div>
       </div>
     </section>
+  <?php endif; ?>
 
     <section id="news-chats">
       <div class="container">
         <div class="row">
           <div class="col-lg-6 d-flex">
-            <div class="promo-block" style="background-image:url(../wp-theme-files/images/news-bg.jpg); background-position:center center;">
+            <div class="promo-block" style="background-image:url(<?php the_field('promo_block_1_background_image'); ?>); <?php the_field('promo_block_1_background_image_css'); ?>">
               <div class="promo-block-caption">
-                <img src="../wp-theme-files/images/news-events-graphic.png" class="img-fluid d-block mx-auto" alt="" />
-                <a href="#" class="btn-main">Click Here</a>
+                <?php the_field('promo_block_1_content'); ?>
               </div>
               <div class="blue-overlay"></div>
             </div>
           </div>
           <div class="col-lg-6 d-flex">
-            <div class="promo-block" style="background-image:url(../wp-theme-files/images/man-talking-to-group.jpg); background-position:center center;">
+            <div class="promo-block" style="background-image:url(<?php the_field('promo_block_2_background_image'); ?>); <?php the_field('promo_block_2_background_image_css'); ?>">
               <div class="promo-block-caption">
-                <img src="../wp-theme-files/images/chats-logo.png" class="img-fluid d-block mx-auto" alt="" />
-                <a href="#" class="childress-chats-register"><span>Register</span>for our next chat</a>
+                <?php the_field('promo_block_2_content'); ?>
               </div>
               <div class="blue-overlay"></div>
             </div>
