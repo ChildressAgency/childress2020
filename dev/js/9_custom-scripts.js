bf5c256294830fc90f914a52863fd57a1c6e3fb9
childressdev <jcampbell@childressagency.com>
2019-12-29 16:44:24 -0500

    small tweaks

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -3,8 +3,45 @@
 */
 
 jQuery(document).ready(function($){
+  var $window = $(window);
+
+  AOS.init();
+
+  // slim header
+  $window.on('scroll', function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      $('#header').addClass('slim');
+    }
+    else {
+      $('#header').removeClass('slim');
+    }
+  });
+
+  // stats (using odometer)
+  $window.on('scroll', function () {
+    if (isInViewport($('#stats'), 100)) {
+      $('.count').each(function (index) {
+        var topNumber = $(this).data('top_number');
+        $(this).text(topNumber);
+      });
+    }
+  });
+
+  // service page center line
+  var servicePageHeight = $('.service-page').outerHeight();
+  var windowHeight = $window.height() - 250; //250 = distance from line start to top of doc
+  var serviceLineContainerHeight = servicePageHeight - windowHeight;
+  var $serviceLine = $('.service-line');
+
+  $window.on('scroll', function () {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var scrolled = (winScroll / serviceLineContainerHeight) * 100;
+    $serviceLine.css('height', scrolled + '%');
+  });
+
+  // swiper events //
   var testimonials = new Swiper('#testimonials .swiper-container',{
-    //autoplay: true,
+    autoplay: true,
     loop: true,
     navigation:{
       nextEl: '.swiper-button-next',
@@ -43,25 +80,7 @@ jQuery(document).ready(function($){
     }
   });
 
-  $(window).on('scroll', function(){
-    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-      $('#header').addClass('slim');
-    }
-    else{
-      $('#header').removeClass('slim');
-    }
-  });
-
-  var servicePageHeight = $('.service-page').outerHeight();
-  var windowHeight = $(window).height() - 250; //250 = distance from line start to top of doc
-  var serviceLineContainerHeight = servicePageHeight - windowHeight;
-  var $serviceLine = $('.service-line');
-  $(window).on('scroll', function(){
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var scrolled = (winScroll / serviceLineContainerHeight) * 100;
-    $serviceLine.css('height', scrolled + '%');
-  });
-
+  // contact form custom checkboxes //
   $('.disc-icon-checkbox input[type="checkbox"]').on('click', function(){
     var $serviceIconBg = $(this).closest('.service').siblings('.disc-icon-bg');
 
@@ -72,24 +91,6 @@ jQuery(document).ready(function($){
       $serviceIconBg.removeClass('service-checked');
     }
   });
-
-  AOS.init();
-
-  $(window).on('scroll', function(){
-    if(isInViewport($('#stats'), 200)){
-      //$('.count').each(function(index){
-      //  var topNumber = $(this).data('top_number');
-      //  var elementId = $(this).attr('id');
-      //  countUp(elementId, topNumber);
-      //});
-
-      $('.count').each(function(index){
-        var topNumber = $(this).data('top_number');
-        $(this).text(topNumber);
-      });
-    }
-  });
-
 }); //end jQuery
 
 
@@ -99,25 +100,11 @@ function isInViewport(element, offset){
   var $window = $(window);
   var viewportTop = $window.scrollTop();
   var viewportHeight = $window.height();
-  var viewportBottom = viewportTop + viewportHeight -100;
+  var viewportBottom = viewportTop + viewportHeight - offset;
   var $element = $(element);
   var top = $element.offset().top;
   var height = $element.height();
   var bottom = top + height;
 
   return (bottom > viewportTop) && (top < viewportBottom);
-}
-
-function countUp(elementId, topNumber){
-  var current = 0;
-  var duration = 5000;
-  var stepTime = Math.abs(Math.floor(duration / topNumber));
-  var $element = $(elementId);
-  var timer = setInterval(function(){
-    current += 1;
-    $element.html(current);
-    if(current == topNumber){
-      clearInterval(timer);
-    }
-  }, stepTime);
 }
