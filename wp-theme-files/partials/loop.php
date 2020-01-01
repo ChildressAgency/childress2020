3e2d5cac373e90a1fbc104e6013a2c504883ef3a
childressdev <jcampbell@childressagency.com>
2019-12-31 20:19:06 -0500

    added

--- wp-theme-files/partials/loop.php
+++ wp-theme-files/partials/loop.php
@@ -1,17 +1,23 @@
-<?php
-  if(have_posts()){
-    while(have_posts()){
-      the_post();
+<section class="main-content">
+  <article class="entry-content">
+    <?php
+      if(have_posts()){
+        while(have_posts()){
+          the_post();
 
-      if(is_singular()){
-        the_content();
+          if(is_singular()){
+            echo '<h1 class="page-title">' . esc_html(get_the_title()) . '</h1>';
+            the_content();
+          }
+          else{
+            echo '<h3><a href="' . esc_url(get_permalink()) . '">' . esc_html(get_the_title()) . '</a></h3>';
+            the_excerpt();
+          }
+        }
       }
       else{
-        echo '<h3><a href="' . esc_url(get_permalink()) . '">' . esc_html(get_the_title()) . '</a></h3>';
-        the_excerpt();
+        echo '<p>' . esc_html__('Sorry, we could not find what you were looking for.' , 'cai') . '</p>';
       }
-    }
-  }
-  else{
-    echo '<p>' . esc_html__('Sorry, we could not find what you were looking for.' , 'cai') . '</p>';
-  }
+    ?>
+  </article>
+</section>
--- wp-theme-files/single.php
+++ wp-theme-files/single.php
@@ -1,3 +1,15 @@
 <?php get_header(); ?>
+<main id="main">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-8 col-lg-9">
+        <?php get_template_part('partials/loop'); ?>
+      </div>
 
+      <div class="col-md-4 col-lg-3">
+        <?php get_sidebar('blog'); ?>
+      </div>
+    </div>
+  </div>
+</main>
 <?php get_footer();
