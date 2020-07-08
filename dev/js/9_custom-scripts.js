217c260f4d93fdf67fa60aa7dad6bfa98c847965
Marina Nicolai <nicolaimarina@gmail.com>
2020-07-08 17:33:42 -0400

    slider

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -1,51 +1,54 @@
 /*!
  * theme custom scripts
-*/
+ */
 
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
   var $window = $(window);
 
   AOS.init();
 
   // slim header
-  $window.on('scroll', function () {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      $('#header').addClass('slim');
-    }
-    else {
-      $('#header').removeClass('slim');
+  $window.on("scroll", function () {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      $("#header").addClass("slim");
+    } else {
+      $("#header").removeClass("slim");
     }
   });
 
   // stats (using odometer)
-  $window.on('scroll', function () {
-    if (isInViewport($('.odo'), 100)) {
-      $('.count').each(function (index) {
-        var topNumber = $(this).data('top_number');
+  $window.on("scroll", function () {
+    if (isInViewport($(".odo"), 100)) {
+      $(".count").each(function (index) {
+        var topNumber = $(this).data("top_number");
         $(this).text(topNumber);
       });
     }
   });
 
   // service page center line
-  var servicePageHeight = $('.service-page').outerHeight();
+  var servicePageHeight = $(".service-page").outerHeight();
   var windowHeight = $window.height() - 250; //250 = distance from line start to top of doc
   var serviceLineContainerHeight = servicePageHeight - windowHeight;
-  var $serviceLine = $('.service-line');
+  var $serviceLine = $(".service-line");
 
-  $window.on('scroll', function () {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  $window.on("scroll", function () {
+    var winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
     var scrolled = (winScroll / serviceLineContainerHeight) * 100;
-    $serviceLine.css('height', scrolled + '%');
+    $serviceLine.css("height", scrolled + "%");
   });
 
   // swiper events //
-  var testimonials = new Swiper('#testimonials .swiper-container',{
+  var testimonials = new Swiper("#testimonials .swiper-container", {
     autoplay: true,
     loop: true,
-    navigation:{
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     slidesPerView: 1,
     spaceBetween: 25,
@@ -54,17 +57,17 @@ jQuery(document).ready(function($){
         slidesPerView: 3,
       },
       992: {
-        slidesPerView: 2
-      }
-    }
+        slidesPerView: 2,
+      },
+    },
   });
 
-  var team = new Swiper('#our-team .swiper-container', {
+  var team = new Swiper("#our-team .swiper-container", {
     //autoplay: true,
     loop: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     slidesPerView: 1,
     spaceBetween: 25,
@@ -72,64 +75,62 @@ jQuery(document).ready(function($){
     //autoHeight: true,
     breakpoints: {
       1200: {
-        slidesPerView: 5
+        slidesPerView: 5,
       },
-      992: { 
-        slidesPerView: 3
-      }
-    }
+      992: {
+        slidesPerView: 3,
+      },
+    },
   });
 
   // contact form custom checkboxes //
-  $('.disc-icon-checkbox input[type="checkbox"]').on('click', function(){
-    var $serviceIconBg = $(this).closest('.service').siblings('.disc-icon-bg');
+  $('.disc-icon-checkbox input[type="checkbox"]').on("click", function () {
+    var $serviceIconBg = $(this).closest(".service").siblings(".disc-icon-bg");
 
-    if($(this).prop("checked") == true){
-      $serviceIconBg.addClass('service-checked');
-    }
-    else{
-      $serviceIconBg.removeClass('service-checked');
+    if ($(this).prop("checked") == true) {
+      $serviceIconBg.addClass("service-checked");
+    } else {
+      $serviceIconBg.removeClass("service-checked");
     }
   });
 
   // ajax loadmore
-  $('body').on('click', '.loadmore', function(e){
+  $("body").on("click", ".loadmore", function (e) {
     e.preventDefault();
     var $clickedButton = $(this);
-    $clickedButton.text('Loading...');
-    var video = $clickedButton.data('video');
+    $clickedButton.text("Loading...");
+    var video = $clickedButton.data("video");
 
     var $loadMoreSection;
-    if(video == "no"){
-      $loadMoreSection = $('.recent-posts');
-    }
-    else{
-      $loadMoreSection = $('.recent-videos');
+    if (video == "no") {
+      $loadMoreSection = $(".recent-posts");
+    } else {
+      $loadMoreSection = $(".recent-videos");
     }
 
     var data = {
-      'action': 'cai_ajax_load_more_posts',
-      'page': cai_ajax_loadmore.page,
-      'video': video,
-      'security': cai_ajax_loadmore.security
+      action: "cai_ajax_load_more_posts",
+      page: cai_ajax_loadmore.page,
+      video: video,
+      security: cai_ajax_loadmore.security,
     };
 
-    $.post(cai_ajax_loadmore.ajaxurl, data, function(response){
-      if(response != ''){
+    $.post(cai_ajax_loadmore.ajaxurl, data, function (response) {
+      if (response != "") {
         $loadMoreSection.append(response);
         cai_ajax_loadmore.page++;
-        $clickedButton.text('Load More');
-      }
-      else{
+        $clickedButton.text("Load More");
+      } else {
         $clickedButton.hide();
       }
     });
   });
 }); //end jQuery
 
-
-function isInViewport(element, offset){
-  if(element.length == 0){ return; }
+function isInViewport(element, offset) {
+  if (element.length == 0) {
+    return;
+  }
 
   var $window = $(window);
   var viewportTop = $window.scrollTop();
@@ -140,5 +141,5 @@ function isInViewport(element, offset){
   var height = $element.height();
   var bottom = top + height;
 
-  return (bottom > viewportTop) && (top < viewportBottom);
+  return bottom > viewportTop && top < viewportBottom;
 }
