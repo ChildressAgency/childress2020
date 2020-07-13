719d88c5036f529993c13a0784cf8ab3ff9eaf09
Marina Nicolai <nicolaimarina@gmail.com>
2020-07-13 12:25:09 -0400

    Last updates

--- dev/js/9_custom-scripts.js
+++ dev/js/9_custom-scripts.js
@@ -143,3 +143,26 @@ function isInViewport(element, offset) {
 
   return bottom > viewportTop && top < viewportBottom;
 }
+
+// swiper events //
+var mySwiper = new Swiper("#learn_more .swiper-container", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
