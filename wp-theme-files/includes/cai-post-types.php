9aa352d65a4720181c55f5aa8b73f6b3ec485473
childressdev <jcampbell@childressagency.com>
2020-08-04 14:32:15 -0400

    added location post type

--- wp-theme-files/includes/cai-post-types.php
+++ wp-theme-files/includes/cai-post-types.php
@@ -60,4 +60,36 @@ function cai_create_post_types(){
       )
     )
   );
+
+  $location_labels = array(
+    'name' => esc_html_x('Locations', 'post type name', 'cai'),
+    'singular_name' => esc_html_x('Location', 'post type singular name', 'cai'),
+    'menu_name' => esc_html_x('Locations', 'post type menu name', 'cai'),
+    'add_new_item' => esc_html__('Add New Location', 'cai'),
+    'search_items' => esc_html__('Search Locations', 'cai'),
+    'edit_item' => esc_html__('Edit Location', 'cai'),
+    'view_item' => esc_html__('View Location', 'cai'),
+    'all_items' => esc_html__('All Locations', 'cai'),
+    'new_item' => esc_html__('New Location', 'cai'),
+    'not_found' => esc_html__('No Locations Found', 'cai')
+  );
+
+  $location_args = array(
+    'labels' => $location_labels,
+    'capability_type' => 'post',
+    'public' => true,
+    'menu_position' => 7,
+    'menu_icon' => 'dashicons-store',
+    'query_var' => 'location',
+    'has_archive' => false,
+    'show_in_rest' => true,
+    'supports' => array(
+      'title',
+      'editor',
+      'custom-fields',
+      'revisions',
+      'thumbnail'
+    )
+  );
+  register_post_type('location', $location_args);
 }
