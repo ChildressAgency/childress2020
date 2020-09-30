951c64367458ce8bb4bda89d1da7a968bd221e99
childressdev <jcampbell@childressagency.com>
2020-09-30 10:01:10 -0400

    added orderby name

--- wp-theme-files/page-locations.php
+++ wp-theme-files/page-locations.php
@@ -15,47 +15,55 @@
 
   <?php
     $states = get_terms(array(
-      'taxonomy' => 'states',
+      'taxonomy' => 'state',
       'hide_empty' => true
     ));
 
     if($states){
-      $city_states = array();
-
+	    echo '<section id="ca-locations"><div class="container">';
+      echo  '<h3>' . esc_html(get_field('locations_list_section_title')) . '</h3>';
+      echo '<ul class="ca-locations">';
+		
       foreach($states as $state){
         $locations = new WP_Query(array(
           'post_type' => 'location',
           'post_status' => 'publish',
+		      'posts_per_page' => -1,
+		      'orderby' => 'name',
+		      'order' => 'ASC',
           'tax_query' => array(
             array(
-              'taxonomy' => 'states',
+              'taxonomy' => 'state',
               'field' => 'slug',
               'terms' => $state->slug
             )
           )
         ));
 
+        $city_states = array();
+        
         if($locations->have_posts()){
+		      $i = 0;
           while($locations->have_posts()){
             $locations->the_post();
 
-            $city_states['city'] = get_the_title();
-            $city_states['state'] = $state->name;
-            $city_states['location_link'] = get_the_permalink();
+            $city_states[$i]['city'] = get_the_title();
+            $city_states[$i]['state'] = $state->name;
+            $city_states[$i]['location_link'] = get_the_permalink();
+			      $i++;
           }
         } wp_reset_postdata();
+		  
+		    //var_dump($city_states);
+      	if(!empty($city_states)){
+		      for($s = 0; $s < count($city_states); $s++){
+            echo '<li><a href="' . $city_state[$s]['location_link'] . '" class="ca-location">' . $city_states[$s]['city'] . ', ' . $city_states[$s]['state'] . '</a></li>';
+          }
+      	}
       }
 
-      if(!empty($city_states)){
-        echo '<section id="ca-locations"><div class="container">';
-          echo  '<h3>' . esc_html(get_field('locations_list_section_title')) . '</h3>';
-          echo '<ul class="ca-locations">';
-            foreach($city_states as $location){
-              echo '<li><a href="' . $location['location_link'] . '" class="ca-location">' . $location['city'] . ', ' . $location['state'] . '</a></li>';
-            }
-          echo '</ul>';
-        echo '</div></section>';
-      }
+	    echo '</ul>';
+      echo '</div></section>';
     }
   ?>
 </main>
